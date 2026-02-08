8c5c31cf94efebc70974fb1601a6ce2834aaef60
shaodowdead-cloud <shaodowdead@gmail.com>
2026-02-08 07:57:26 +0200

    Fix missing Inventory import

--- src/main/java/gg/auroramc/potionaddon/gui/QuestGuiListener.java
+++ src/main/java/gg/auroramc/potionaddon/gui/QuestGuiListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerEditBookEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
